6d727591cdc3e8a0bc88057d4fd60d2c4fdd965b
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2016-07-14 14:10:20 +0200

    travis

---
+++ tests/bootstrap.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once dirname(dirname(__FILE__)) . "/autoloader.php";
+
+require_once \Elgg\Application::root() . 'engine/tests/phpunit/bootstrap.php';
---
+++ tests/phpunit/classes/hypeJunction/hypeAttachments/SampleTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace hypeJunction\hypeAttachments;
+
+class SampleTest extends \PHPUnit_Framework_TestCase {
+
+	function testCanTest() {
+		$this->asserTrue(true);
+	}
+
+}
