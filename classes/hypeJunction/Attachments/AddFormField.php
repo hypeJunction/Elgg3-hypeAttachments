ad09845bf656e3e0de18b3809427f10cc56931e2
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2018-03-28 12:11:55 +0200

    feat(fields): upgrade to new fields API

--- classes/hypeJunction/Attachments/AddFormField.php
+++ classes/hypeJunction/Attachments/AddFormField.php
@@ -2,43 +2,30 @@
 
 namespace hypeJunction\Attachments;
 
-use Elgg\Request;
+use Elgg\Hook;
+use hypeJunction\Fields\Collection;
+use InvalidParameterException;
 
 class AddFormField {
 
 	/**
 	 * Add field
 	 *
-	 * @param \Elgg\Hook $hook Hook
+	 * @param Hook $hook Hook
 	 *
-	 * @return \ElggMenuItem[]|null
+	 * @return Collection
+	 * @throws InvalidParameterException
 	 */
-	public function __invoke(\Elgg\Hook $hook) {
+	public function __invoke(Hook $hook) {
 
 		$fields = $hook->getValue();
+		/* @var $fields \hypeJunction\Fields\Collection */
 
-		$fields['attachments'] = [
-			'#type' => 'attachments',
-			'#section' => 'content',
-			'#priority' => 700,
-			'#input' => function(Request $request) {
-				return elgg_get_uploaded_files('attachments');
-			},
-			'#getter' => function(\ElggEntity $entity) {
-				return hypeapps_get_attachments($entity);
-			},
-			'#setter' => function(\ElggEntity $entity) {
-				return hypeapps_attach_uploaded_files($entity, 'attachments', [
-					'access_id' => $entity->access_id,
-					'container_guid' => $entity->guid,
-					'origin' => 'cms',
-				]);
-			},
-			'#visibility' => function (\ElggEntity $entity) {
-				return hypeapps_allow_attachments($entity->type, $entity->subtype);
-			},
-			'#profile' => false,
-		];
+		$fields->add('attachments', new AttachmentsField([
+			'type' => 'attachments',
+			'priority' => 700,
+			'is_profile_field' => false,
+		]));
 
 		return $fields;
 	}
